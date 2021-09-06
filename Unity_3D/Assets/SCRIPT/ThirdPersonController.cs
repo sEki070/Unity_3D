e54f68ef433f6b6d95df97375eacbee983f7f5b0
seki070 <4A4E0040@stust.edu.tw>
2021-09-06 15:01:06 +0800

    認識屬性與方法

--- Unity_3D/Assets/SCRIPT/ThirdPersonController.cs
+++ Unity_3D/Assets/SCRIPT/ThirdPersonController.cs
@@ -22,9 +22,9 @@ public class ThirdPersonController : MonoBehaviour
     [Header("跳躍高度"), Tooltip("用來調整腳色跳躍高度"), Range(1, 1000)]
     public int hight = 100;
 
-    [Header("檢查地面資料") ]
+    [Header("檢查地面資料")]
     [Tooltip("用來檢查腳色是否在地面上")]
-    public bool on_the_ground ;
+    public bool on_the_ground;
     public float groundhannkei = 0.2f;
     public Vector3 v3CheckGroundOffset;
     [Range(0, 3)]
@@ -88,14 +88,146 @@ public class ThirdPersonController : MonoBehaviour
 
     #endregion
     #endregion
-    #region 屬性Property
-    #endregion
 
+    #region 屬性Property
+
+    //屬性不會顯示在面板上
+    //儲存資料，與欄位相同
+    //差異在於:可以設定存取全縣 Get Set
+    //屬性語法:修飾詞 資料類型 屬性名稱 {取;存;}
+    public int readAndWrite { get; set; }
+    //唯讀屬性:只能取得Get
+    public int read { get; }
+    //唯讀屬性: 透過get設定預設值，關鍵字return為傳回值
+    public int readValue
+    {
+        get
+        {
+            return 77;
+        }
+    }
+    //唯寫屬性是禁止的，必須要有get
+    //public int write { set; }
+    //value 指的是指定的值
+    private int _hp;
+    public int hp
+    {
+        get
+        {
+            return _hp;
+        }
+        set
+        {
+            _hp = value;
+        }
+
+    }
+    #endregion
+    public KeyCode keyJump { get; }
     #region 方法 Method
+    //定義與實作較複雜程式的區塊，功能
+    //方法語法:修飾詞 傳回資料類型 方法名稱(參數1,....參數N){程式區塊}
+    //常用傳回類型: 無傳回void - 此方法沒有傳回資料
+    //格式化:ctrl+K、D
+    //自訂方法:
+    //自訂方法需要被呼叫才會執行方法內的程式
+    //名稱顏色為淡黃色 - 沒有被呼叫
+    //名稱顏色為亮黃色 - 有被呼叫
+    private void Test()
+    {
+        print("我是自訂方法");
+    }
+
+    private int ReturnJump()
+    {
+        return 999;
+    }
+
+    //參數語法:資料類型 參數名稱
+    private void Skill(int damage)
+    {
+        print("參數版本 - 傷害值:" + damage);
+        print("參數版本-技能特效");
+    }
+
+    //對照組:不使用參數
+    //降低維護擴充性
+    private void Skill100()
+    {
+        print("傷害值:" + 100);
+        print("技能特效");
+    }
+    private void Skill200()
+    {
+        print("傷害值:" + 200);
+        print("技能特效");
+    }
+    private void Skill300()
+    {
+        print("傷害值:" + 300);
+        print("技能特效");
+    }
+
     #endregion
 
     #region 事件 Event
     //特定時間點會執行的方法，程式的入口start等於Console Main
+    //開始事件:遊戲開始時執行一次，處理初始化，取得資料等等
+    private void Start()
+    {
+        Skill100();
+        Skill200();
+        //呼叫有參數方法時，必須輸入對應的引數
+        Skill(300);
+        Skill(999);
+
+
+        #region 輸出 方法
+        /*
+        print("哈囉");
+
+        Debug.Log("一般訊息");
+        Debug.LogWarning("警告訊息");
+        Debug.LogError("錯誤訊息");
+        */
+        #endregion
+
+        # region 屬性練習
+        /** 屬性練習
+        //欄位與屬性 取得Gt，設定Set
+        print("欄位資料 - 移動速度:" + speed);
+        print("屬性資料 - 讀寫屬性:" + readAndWrite);
+        speed = 20.5f;
+        readAndWrite = 90;
+        print("修改後的資料");
+        print("欄位資料 - 移動速度:" + speed);
+        print("屬性資料 - 讀寫屬性:" + readAndWrite);
+        //唯讀屬性
+        //read = 7  //唯讀屬性不能設定set
+        print("唯讀屬性:" + read);
+        print("唯讀屬性，有預設值:" + readValue);
+
+        //屬性存取練習
+        print("HP :" + hp);
+        hp = 100;
+        print("HP :" + hp);
+        /**/
+        #endregion
+        //呼叫自訂方法語法:方法名稱();
+        Test();
+        //呼叫有傳回值的方法
+        //1.區域變數指定傳回值 - 區域變數僅能在此結構(大括號)內存取
+        int j = ReturnJump();
+        print("跳躍值:" + j);
+        //2.將傳回方法當成值使用
+        print("跳躍值，當值使用:" + (ReturnJump() + 1));
+    }
+    //更新事件:一秒執行60次.60FPS - Frame Per Second
+    //處理持續性的運動，移動物件，監聽玩家輸入按鍵
+    private void Update()
+    {
+
+    }
     #endregion
 
 }
