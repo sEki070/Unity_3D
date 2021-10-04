341f60fa4251779b30636e93dddaa2fdbe9c124e
seki070 <4A4E0040@stust.edu.tw>
2021-10-04 14:48:46 +0800

    完成腳色控制器和方向

--- Unity_3D/Assets/SCRIPT/ThirdPersonCamera.cs
+++ Unity_3D/Assets/SCRIPT/ThirdPersonCamera.cs
@@ -18,25 +18,61 @@ namespace Shih
         public float speedTurnHorizontal = 5;
         [Header("旋轉上下速度"), Range(0, 100)]
         public float speedTurnVertical = 5;
+        [Header("在X軸上下旋轉限制: 最小與最大值")]
+        public Vector2 limitAngleX = new Vector2(-0.2f, 0.2f);
+
+        /// <summary>
+        /// 攝影機前方座標
+        /// </summary>
+        private Vector3 _posForward;
+        /// <summary>
+        /// 前方的長度
+        /// </summary>
+        private float lengthForward;
         #endregion
 
         #region 屬性
         /// <summary>
         /// 取得滑鼠水平座標
         /// </summary>
-        public float inputMouseX { get=>Input.GetAxis("Mouse X"); }
+        public float inputMouseX { get => Input.GetAxis("Mouse X"); }
         /// <summary>
         /// 取得滑鼠垂直座標
         /// </summary>
-        public float inputMouseY { get=>Input.GetAxis("Mouse Y"); }
+        public float inputMouseY { get => Input.GetAxis("Mouse Y"); }
 
+        public Vector3 posForward
+        {
+            get
+            {
+                _posForward = transform.position + transform.forward * lengthForward;
+                _posForward.y = target.position.y;
+                return _posForward;
+            }
+        }
         #endregion
 
         #region 事件
         //在Update 後執行，處理攝影機追蹤行為
+        private void Update()
+        {
+            TurnCamera();
+            LimitAngleX();
+            FreezeAngleZ();
+        }
         private void LateUpdate()
         {
             TrackTarget();
+        }
+        //在執行檔不會執行的事件
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = new Color(0.2f, 0, 1, 0.3f);
+            //前方座標 = 此物件座標+此物件前方*長度
+            _posForward = transform.position + transform.forward * lengthForward;
+            //前方座標.y = 目標.座標.y(讓前方座標的高度與目標相同)
+            _posForward.y = target.position.y;
+            Gizmos.DrawSphere(_posForward, 0.15f);
         }
         #endregion
 
@@ -44,16 +80,13 @@ namespace Shih
         /// <summary>
         /// 追蹤目標
         /// </summary>
-        private void Update()
-        {
-            TurnCamera();
-        }
+
         private void TrackTarget()
         {
             Vector3 posTarget = target.position;                          //取得 目標 座標
             Vector3 posCamera = transform.position;                       //取得 攝影機 座標
-             //攝影機座標 = 插值 (速度*一幀的時間)
-            posCamera = Vector3.Lerp(posCamera,posTarget,  speedTrack*Time.deltaTime);   
+                                                                          //攝影機座標 = 插值 (速度*一幀的時間)
+            posCamera = Vector3.Lerp(posCamera, posTarget, speedTrack * Time.deltaTime);
 
             transform.position = posCamera;                               //此物件的座標 = 攝影機座標
         }
@@ -64,8 +97,23 @@ namespace Shih
         private void TurnCamera()
         {
             transform.Rotate(
-            inputMouseY* Time.deltaTime*speedTurnHorizontal,
-            inputMouseX* Time.deltaTime*speedTurnHorizontal,0);
+            inputMouseY * Time.deltaTime * speedTurnVertical,
+            inputMouseX * Time.deltaTime * speedTurnHorizontal, 0);
+
+        }
+
+        private void LimitAngleX()
+        {
+            //print("攝影機的角度資訊 "+transform.rotation);
+            Quaternion angle = transform.rotation;
+            angle.x = Mathf.Clamp(angle.x, limitAngleX.x, limitAngleX.y);
+            transform.rotation = angle;
+        }
+        private void FreezeAngleZ()
+        {
+            Vector3 angle = transform.eulerAngles;
+            angle.z = 0;
+            transform.eulerAngles = angle;
         }
         #endregion
     }
