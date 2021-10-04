542c930dabc98ff43c848c5a0f377365f1936158
seki070 <4A4E0040@stust.edu.tw>
2021-10-04 11:59:58 +0800

    攝影機追蹤

--- Unity_3D/Assets/SCRIPT/ThirdPersonCamera.cs
+++ Unity_3D/Assets/SCRIPT/ThirdPersonCamera.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+
 namespace Shih
 {
     /// <summary>
@@ -13,21 +14,60 @@ namespace Shih
         public Transform target;
         [Header("追蹤速度"), Range(0, 500)]
         public float speedTrack = 1.5f;
+        [Header("旋轉左右速度"), Range(0, 100)]
+        public float speedTurnHorizontal = 5;
+        [Header("旋轉上下速度"), Range(0, 100)]
+        public float speedTurnVertical = 5;
         #endregion
 
         #region 屬性
+        /// <summary>
+        /// 取得滑鼠水平座標
+        /// </summary>
+        public float inputMouseX { get=>Input.GetAxis("Mouse X"); }
+        /// <summary>
+        /// 取得滑鼠垂直座標
+        /// </summary>
+        public float inputMouseY { get=>Input.GetAxis("Mouse Y"); }
+
         #endregion
 
         #region 事件
+        //在Update 後執行，處理攝影機追蹤行為
+        private void LateUpdate()
+        {
+            TrackTarget();
+        }
         #endregion
 
         #region 方法
+        /// <summary>
+        /// 追蹤目標
+        /// </summary>
+        private void Update()
+        {
+            TurnCamera();
+        }
+        private void TrackTarget()
+        {
+            Vector3 posTarget = target.position;                          //取得 目標 座標
+            Vector3 posCamera = transform.position;                       //取得 攝影機 座標
+             //攝影機座標 = 插值 (速度*一幀的時間)
+            posCamera = Vector3.Lerp(posCamera,posTarget,  speedTrack*Time.deltaTime);   
+
+            transform.position = posCamera;                               //此物件的座標 = 攝影機座標
+        }
+
+        /// <summary>
+        /// 旋轉攝影機
+        /// </summary>
+        private void TurnCamera()
+        {
+            transform.Rotate(
+            inputMouseY* Time.deltaTime*speedTurnHorizontal,
+            inputMouseX* Time.deltaTime*speedTurnHorizontal,0);
+        }
         #endregion
-
-
-
-
-
     }
 }
 
